7263e278cb607e6714e879f9ade24b09e4941f0a
hameedsk381 <hameedsk381@gmail.com>
2024-02-24 09:33:15 +0530

    removed env

--- src/apis/extractApis.js
+++ src/apis/extractApis.js
@@ -1,6 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-export const serverApi = process.env.SERVER_URL
+
+export const serverApi = 'https://renarration-api.onrender.com'
 export const extractApi = `${serverApi}/download`;
 export const submitApi = `${serverApi}/sweets/create-renarration`;
 export const getAllRenarrations = `${serverApi}/sweets/renarrations`;
