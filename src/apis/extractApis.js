46c2ab2d1b93ef89c0ac4c725701058dfc46cc0e
hameed Shaik <hameedsk381@gmail.com>
2024-03-07 10:47:10 +0530

    severapi updated

--- src/apis/extractApis.js
+++ src/apis/extractApis.js
@@ -1,5 +1,5 @@
 
-export const serverApi = 'https://reanarration-fastify-api.onrender.com'
+export const serverApi = 'https://renarration-api-test.vercel.app/'
 // export const serverApi = 'http://localhost:4000'
 export const extractApi = `${serverApi}/download`;
 export const submitApi = `${serverApi}/sweets/create-renarration`;
